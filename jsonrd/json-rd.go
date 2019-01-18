65c31cb1c0ca7280908286733bc3d6fc0eb6c74e
julio <julioolivares90@gmail.com>
2019-01-18 12:47:30 -0600

    mejoras

--- jsonrd/json-rd.go
+++ jsonrd/json-rd.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
 )
 
 func JsonReader() {
@@ -20,6 +21,24 @@ func JsonReader() {
 	var rs map[string]interface{}
 	json.Unmarshal([]byte(byteValue), &rs)
 	fmt.Println(rs["users"])
+}
+
+func JsonDecode() {
+	jsonFile, err := os.Open("usrs.json")
+	if err != nil {
+		fmt.Printf("error : %s", err)
+	}
+	defer jsonFile.Close()
+	var usrs Usrs
+	byt3, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byt3, &usrs)
+	for index := 0; index < len(usrs.Usrs); index++ {
+		fmt.Println("User Type: " + usrs.Usrs[index].Type)
+		fmt.Println("User Age: " + strconv.Itoa(usrs.Usrs[index].Age))
+		fmt.Println("User Name: " + usrs.Usrs[index].Name)
+		fmt.Println("Facebook Url: " + usrs.Usrs[index].Social.Facebook)
+		fmt.Println("Twitter Url:" + usrs.Usrs[index].Social.Twitter)
+	}
 }
 
 type Usrs struct {
